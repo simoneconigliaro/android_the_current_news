007545461e96f1010f6bee736952dc210155bb62
Simone Conigliaro <33692787+simoneconigliaro@users.noreply.github.com>
2018-03-31 11:49:32 +0100

    Added logo and viewpager with three fragment.

---
+++ app/src/main/java/com/project/simoneconigliaro/thecurrentnews/ui/FavoriteNewsFragment.java
@@ -0,0 +1,31 @@
+package com.project.simoneconigliaro.thecurrentnews.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.simoneconigliaro.thecurrentnews.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FavoriteNewsFragment extends Fragment {
+
+
+    public FavoriteNewsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_favorite_news, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/project/simoneconigliaro/thecurrentnews/ui/MainActivity.java
@@ -0,0 +1,39 @@
+package com.project.simoneconigliaro.thecurrentnews.ui;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.simoneconigliaro.thecurrentnews.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String GLOBAL_NEWS = "Global";
+    public static final String LOCAL_NEWS = "Local";
+    public static final String FAVORITE_NEWS = "Favorite";
+
+    @BindView(R.id.view_pager)
+    ViewPager viewPager;
+
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new GlobalNewsFragment(), GLOBAL_NEWS);
+        viewPagerAdapter.addFragment(new LocalNewsFragment(), LOCAL_NEWS);
+        viewPagerAdapter.addFragment(new FavoriteNewsFragment(), FAVORITE_NEWS);
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
