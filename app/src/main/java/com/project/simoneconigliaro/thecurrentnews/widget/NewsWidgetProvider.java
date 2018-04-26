78ffd8fc7e653c2ec37e1ab4fdb42ed35eec596a
Simone Conigliaro <33692787+simoneconigliaro@users.noreply.github.com>
2018-04-26 21:51:37 +0100

    Improved UI widget, added onClickListener to the list items widget.

--- app/src/main/java/com/project/simoneconigliaro/thecurrentnews/widget/NewsWidgetProvider.java
+++ app/src/main/java/com/project/simoneconigliaro/thecurrentnews/widget/NewsWidgetProvider.java
@@ -6,9 +6,11 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
 import android.widget.RemoteViews;
 
 import com.project.simoneconigliaro.thecurrentnews.R;
+import com.project.simoneconigliaro.thecurrentnews.ui.DetailArticleActivity;
 import com.project.simoneconigliaro.thecurrentnews.ui.MainActivity;
 
 /**
@@ -19,14 +21,12 @@ public class NewsWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.news_widget_provider);
 
         Intent intent = new Intent(context, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        //views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -44,8 +44,19 @@ public class NewsWidgetProvider extends AppWidgetProvider {
         for (int appWidgetId : appWidgetIds) {
 
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.news_widget_provider);
+
             Intent intent = new Intent(context, WidgetService.class);
             remoteViews.setRemoteAdapter(R.id.widget_list_view, intent);
+
+            // template to handle the click listener for each item
+            Intent clickIntentTemplate = new Intent(context, DetailArticleActivity.class);
+            PendingIntent clickPendingIntentTemplate = TaskStackBuilder.create(context)
+                    .addNextIntentWithParentStack(clickIntentTemplate)
+                    .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setPendingIntentTemplate(R.id.widget_list_view, clickPendingIntentTemplate);
+
+
+
             appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
         }
     }
--- app/src/main/java/com/project/simoneconigliaro/thecurrentnews/widget/WidgetFactory.java
+++ app/src/main/java/com/project/simoneconigliaro/thecurrentnews/widget/WidgetFactory.java
@@ -3,19 +3,25 @@ package com.project.simoneconigliaro.thecurrentnews.widget;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.os.Binder;
 import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.project.simoneconigliaro.thecurrentnews.R;
+import com.project.simoneconigliaro.thecurrentnews.data.Article;
 import com.project.simoneconigliaro.thecurrentnews.data.ArticleContract;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 public class WidgetFactory implements RemoteViewsService.RemoteViewsFactory {
 
     private Context mContext;
     private Cursor mCursor;
 
+    private final static String ARTICLE_KEY = "article";
 
     public WidgetFactory(Context context, Intent intent) {
         this.mContext = context;
@@ -63,6 +69,27 @@ public class WidgetFactory implements RemoteViewsService.RemoteViewsFactory {
 
         RemoteViews remoteViews = new RemoteViews(mContext.getPackageName(), R.layout.widget_list_item);
         remoteViews.setTextViewText(R.id.tv_title_widget, mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_TITLE)));
+
+        String urlToImage = mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_URL_IMAGE));
+
+        try {
+            Bitmap bitmap = Picasso.get().load(urlToImage).get();
+            remoteViews.setImageViewBitmap(R.id.iv_article_widget, bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String name = mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_NAME));
+        String title = mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_TITLE));
+        String url = mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_URL));
+        String urlImage = mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_URL_IMAGE));
+        String date = mCursor.getString(mCursor.getColumnIndex(ArticleContract.ArticleEntry.COLUMN_DATE));
+        Article article = new Article(name, title, url, urlImage, date);
+
+        Intent fillInIntent = new Intent();
+        fillInIntent.putExtra(ARTICLE_KEY, article);
+        remoteViews.setOnClickFillInIntent(R.id.widget_item_container, fillInIntent);
+
         return remoteViews;
     }
 
